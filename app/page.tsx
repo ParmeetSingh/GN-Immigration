9fff8cf05bd9a49ff5cfb8e47f1a5e41b1345f5c
gurjot-07 <gurjotsingh.gs2000@gmail.com>
2024-09-03 01:58:27 -0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ const Home = () => {
           
           <div className="h-[40vh] md:h-[30vh] flex flex-col md:flex-row items-center justify-around px-6 md:px-10 text-center">
             <div className="flex-1 flex flex-col justify-center items-center p-4 md:p-8 rounded-lg m-4">
-              <h2 className="text-xl md:text-3xl font-bold text-gray-800 mb-4">Don't Just Take Our Word For It</h2>
+              <h2 className="text-xl md:text-3xl font-bold text-gray-800 mb-4">Dont Just Take Our Word For It</h2>
               <p className="text-lg md:text-xl text-gray-600 mb-6">See what our clients are saying about us</p>
               <ul className="text-lg md:text-2xl text-gray-700 space-y-2">
                 <li>100+ clients served</li>
